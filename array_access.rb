9f1c392b66a5541ad350151b8eb2f48fb0e8db3e
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2018-08-12 12:40:32 -0400

    init with ruby array access

---
+++ array_access.rb
@@ -0,0 +1,30 @@
+require 'benchmark/ips'
+
+@a = (1..1_000_000).to_a
+@linear = (0..999_999).to_a
+@rand = (0..999_999).to_a.shuffle
+
+def linear_access 
+  count = 0 
+  @linear.each do |x|
+    @a[x]
+    count += 1
+  end
+  count
+end
+
+def random_access
+  count = 0 
+  @rand.each do |x|
+    @a[x]
+    count += 1
+  end
+  count
+end
+
+Benchmark.ips do |x|
+  x.report('linear access: ') { linear_access }
+  x.report('random access: ')   { random_access }
+
+  x.compare!
+end
